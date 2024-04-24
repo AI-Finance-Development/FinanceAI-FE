362c2d73cdc3a0396e17741a51faea66911fc4f2
yagmurfdan <yagmurfdann@outlook.com>
2024-04-25 00:41:30 +0300

    created navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
-const navbar = () => {
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Row, Col, Space } from 'antd';
+import logo from './logo.png';
+import './navbar.css';
+const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <Row justify="space-around" className="containerNav">
+        <Col ><img className='logo' src={logo} alt="Logo"  /></Col> 
+          <Col>
+            <Space size={'large'}>
+              <Link  to="/">Ana Sayfa</Link>
+              <Link to="/kullanma-kilavuzu">Nasıl Kullanılır ?</Link>
+              <Link to="/merak-ettikleriniz">Merak ettikleriniz</Link>
+              <Link to="/neden-biz">Neden biz ?</Link>
+            </Space>
+          </Col>
+        </Row>
+      </div>
+    </Router>
+  );
+};
 
-export default navbar
+export default Navbar;
