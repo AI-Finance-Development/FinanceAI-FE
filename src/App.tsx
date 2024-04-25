a2f518bbb93a61582cb174db44ef60297faa8618
yagmurfdan <yagmurfdann@outlook.com>
2024-04-25 13:27:28 +0300

    fixed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from './components/navbar/Navbar';
 import './App.css';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-   <Navbar/>
+      <Navbar />
     </div>
   );
 }
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Row, Col, Space } from 'antd';
-import logo from './logo.png';
+import navbarLogo from '../../assets/navbar-logo.png';
 import './navbar.css';
 const Navbar = () => {
   return (
-    <Router>
-      <div>
-        <Row justify="space-around" className="containerNav">
-        <Col ><img className='logo' src={logo} alt="Logo"  /></Col> 
-          <Col>
-            <Space size={'large'}>
-              <Link  to="/">Ana Sayfa</Link>
-              <Link to="/kullanma-kilavuzu">Nasıl Kullanılır ?</Link>
-              <Link to="/merak-ettikleriniz">Merak ettikleriniz</Link>
-              <Link to="/neden-biz">Neden biz ?</Link>
-            </Space>
-          </Col>
-        </Row>
-      </div>
-    </Router>
+    <Row justify="space-around" className="containerNav">
+      <Col><img className='logo' src={navbarLogo} alt="Logo" /></Col>
+      <Col>
+        <Space size={'large'}>
+          <span >Ana Sayfa</span>
+          <span >Nasıl Kullanılır ?</span>
+          <span >Merak ettikleriniz</span>
+          <span >Neden biz ?</span>
+        </Space>
+      </Col>
+    </Row>
   );
 };
 
