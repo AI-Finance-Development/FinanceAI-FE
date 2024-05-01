fb6e5569e9bfb7ecf6a2ed5c492fd805e2204d21
yagmurfdan <yagmurfdann@outlook.com>
2024-05-02 00:41:54 +0300

    added localization to navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { Row, Col, Space } from 'antd';
 import navbarLogo from '../../assets/navbar-logo.png';
 import './navbar.css';
+import { useTranslation } from 'react-i18next';
 const Navbar = () => {
+  const{t}=useTranslation();
+
+  
   return (
     <Row justify="space-around" className="containerNav">
       <Col><img className='logo' src={navbarLogo} alt="Logo" /></Col>
       <Col>
         <Space size={'large'}>
-          <span >Ana Sayfa</span>
-          <span >Nasıl Kullanılır ?</span>
-          <span >Merak ettikleriniz</span>
-          <span >Neden biz ?</span>
+          <span >{t('components.navbar.homepage')}</span> 
+          <span >{t('components.navbar.howToUse')}</span>
+          <span >{t('components.navbar.thingsYouWonder')}</span>
+          <span >{t('components.navbar.WhyUs')}</span>
         </Space>
       </Col>
     </Row>
