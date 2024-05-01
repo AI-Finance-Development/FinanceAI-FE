dfb676b553019c1b547f9d9abed9229bd2e30b36
eneskadumi <eneskadumi@gmail.com>
2024-05-02 00:54:22 +0300

    added css auth page

---
+++ src/components/secondary-button/secondary-button.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./secondary-button.css";
+import { Button, ButtonProps, ConfigProvider } from "antd";
+import { ReactComponent as GoogleSVG } from "../../assets/google.svg";
+import { GoogleOutlined } from "@ant-design/icons";
+interface SecondaryButtonProps extends ButtonProps {
+    buttonText: string;
+}
+
+const SecondaryButton = (props: SecondaryButtonProps) => {
+    return (
+        <ConfigProvider
+            theme={{
+                components: {
+                    Button: {
+                        borderRadius: 15,
+                        primaryColor: "rgb(255, 255, 255)",
+                        colorPrimary: "rgb(40, 52, 82)",
+                        colorPrimaryHover: "rgb(77, 94, 137)",
+                        defaultBg: "rgb(255, 255, 255)",
+                        defaultColor: "rgba(0, 0, 0, 0.88)",
+                        defaultBorderColor: "rgb(255, 255, 255)",
+                        defaultHoverBg: "rgb(85, 98, 131)",
+                        defaultHoverBorderColor: "rgb(76, 91, 126)",
+                        defaultHoverColor: "rgb(255, 255, 255)",
+
+                    },
+                },
+            }}
+        >
+            <Button {...props} className="button">
+                {props.buttonText}
+            </Button>
+        </ConfigProvider>
+    );
+};
+
+export default SecondaryButton;
--- src/page-parts/auth-part/auth-part.tsx
+++ src/page-parts/auth-part/auth-part.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import './auth-part.css'
-import {  Space } from 'antd'
-import PrimaryButton from '../../components/primary-button/primary-button'
-import Divider from '../../components/divider/divider'
+import React from "react";
+import "./auth-part.css";
+import { Space } from "antd";
+import PrimaryButton from "../../components/primary-button/primary-button";
+import Divider from "../../components/divider/divider";
+import SecondaryButton from "../../components/secondary-button/secondary-button";
+import { GoogleOutlined } from '@ant-design/icons';
 
 const AuthPart = () => {
     return (
-        <div className='right-container'>
-            <div className='header-title'>
-                <span>
-                    HEMEN HEDEFLERİNE BAŞLA
+        <div className="right-container">
+            <div className="header-title">
+                <span className="sp1">
+                    HEMEN <span className="sp2">HEDEFLERİNE</span> BAŞLA
                 </span>
             </div>
-            <div className='header-body'>
-                <Space direction='vertical' size={'small'}>
-                    <PrimaryButton buttonText='Google İle Giriş'></PrimaryButton>
-                    <Divider/>
-                    <PrimaryButton buttonText='Google İle Giriş'></PrimaryButton>
-                </Space>
-                <div className='right-footer'>
+            <div className="header-body">
+                    <Space direction="vertical" size={"small"}>
+                        <SecondaryButton className="buttons" icon={<GoogleOutlined />} buttonText="Google ile giriş yap"></SecondaryButton>
+                        <Divider />
+                        <SecondaryButton className="buttons" buttonText="Hesap Oluştur"></SecondaryButton>
+                    </Space>
+                <div className="right-footer">
                     <span>Zaten hesabın var mı?</span>
-                    <PrimaryButton buttonText='Google İle Giriş'></PrimaryButton>
+                    <PrimaryButton buttonText="Giriş yap"></PrimaryButton>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AuthPart
+export default AuthPart;
