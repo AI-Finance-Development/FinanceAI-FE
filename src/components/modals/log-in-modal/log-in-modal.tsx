529152c5e2e795d25ed0beaa065a9392a8c73009
Okan Cezik <okacezik@gmail.com>
2024-05-07 00:02:59 +0300

    added form label component

--- src/components/modals/log-in-modal/log-in-modal.tsx
+++ src/components/modals/log-in-modal/log-in-modal.tsx
@@ -4,13 +4,14 @@ import { Form, Modal, ModalProps, Space } from 'antd'
 import FaiInput from '../../atomics/fai-input/fai-input'
 import PrimaryButton from '../../atomics/primary-button/primary-button'
 import { useTranslation } from 'react-i18next'
+import FormLabel from '../../atomics/form-label/form-label'
 
 
 interface LogInModalProps extends ModalProps { }
 
 const LogInModal = (props: LogInModalProps) => {
 
-    const {t} = useTranslation();
+    const { t } = useTranslation();
 
     const onFinish = (values: any) => {
         console.log("values: ", values)
@@ -24,12 +25,16 @@ const LogInModal = (props: LogInModalProps) => {
                     <span className='subTitle'>Please enter your info</span>
                 </Space>
                 <Form layout='vertical' style={{ marginTop: "1.5rem" }} onFinish={onFinish}>
-                    <Form.Item label="Kullanıcı Adı" name={"username"}>
-                        <FaiInput size='large' />
-                    </Form.Item>
-                    <Form.Item label="Şifre" name={"password"}>
-                        <FaiInput size='large' />
-                    </Form.Item>
+                    <FormLabel label='Kullanıcı Adı'>
+                        <Form.Item name={"username"}>
+                            <FaiInput size='large' />
+                        </Form.Item>
+                    </FormLabel>
+                    <FormLabel label='Şifre'>
+                        <Form.Item name={"password"}>
+                            <FaiInput size='large' />
+                        </Form.Item>
+                    </FormLabel>
                     <Form.Item>
                         <PrimaryButton htmlType='submit' buttontext={t('page-parts.auth-part.logIn')}></PrimaryButton>
                     </Form.Item>
