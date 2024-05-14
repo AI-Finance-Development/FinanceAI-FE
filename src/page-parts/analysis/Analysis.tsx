691ebeebbd044c2b53f75b3c1e8165cc34616396
yagmurfdan <yagmurfdann@outlook.com>
2024-05-15 00:21:56 +0300

    css changed

--- src/page-parts/analysis/Analysis.tsx
+++ src/page-parts/analysis/Analysis.tsx
@@ -2,6 +2,8 @@ import { Row,Image,Col } from 'antd'
 import React from 'react'
 import grafik from '../../assets/grafik.png';
 import './analysis.css'
+import PrimaryButton from '../../components/atomics/primary-button/primary-button';
+import SecondaryButton from '../../components/atomics/secondary-button/secondary-button';
 const Analysis = () => {
   return (
     <div>
@@ -10,9 +12,9 @@ const Analysis = () => {
             <Col xs={0} sm={0} md={0} lg={9} className='left-c'>
                 <Image preview={false} src={grafik} alt='grafik' />
             </Col>
-            <Col sm={24} md={24} lg={5} className='auth-c'>
-                <button className='buton1'>Hedef Oluştur</button>
-                <button className='buton2'>Hedefleri gör</button>
+            <Col sm={24} md={24} lg={6} className='auth-c'>
+                <PrimaryButton className='buton1' buttontext='Hedefleri Gör' size='large'></PrimaryButton>
+                <PrimaryButton className='buton2'buttontext='Hedef Oluştur'></PrimaryButton>
             </Col>
             <Col lg={3}></Col>
         </Row>
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './index.css';
 import HowItWorks from '../../page-parts/how-it-works/how-it-works';
 import Startpage from '../../page-parts/start-page/start-page';
+import Analysis from '../../page-parts/analysis/Analysis';
 
 
 const Index = () => {
   return ( 
     <>
-   
+   <Analysis/>
    <Startpage/> 
    <br></br>
    <br></br>
