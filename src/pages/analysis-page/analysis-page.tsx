33ff70b525d4ad0f6ec9bacccc4cb37518da1717
Okan Cezik <okacezik@gmail.com>
2024-05-15 01:42:24 +0300

    removed unreferenced

--- src/pages/analysis-page/analysis-page.tsx
+++ src/pages/analysis-page/analysis-page.tsx
@@ -3,31 +3,27 @@ import React from "react";
 import grafik from "../../assets/grafik.png";
 import "./analysis-page.css";
 import PrimaryButton from "../../components/atomics/primary-button/primary-button";
-import SecondaryButton from "../../components/atomics/secondary-button/secondary-button";
-import Navbar from "../../layouts/navbar/navbar";
+
 const Analysis = () => {
   return (
-    
-     
-      <Row className="Analiz">
-        <Col lg={3}></Col>
-        <Col xs={0} sm={0} md={0} lg={9} className="left-c">
-          <Image preview={false} src={grafik} alt="grafik" />
-        </Col>
-        <Col sm={24} md={24} lg={6} className="auth-c">
-          <PrimaryButton
-            className="buton1"
-            buttontext="Hedefleri Gör"
-            size="large"
-          ></PrimaryButton>
-          <PrimaryButton
-            className="buton2"
-            buttontext="Hedef Oluştur"
-          ></PrimaryButton>
-        </Col>
-        <Col lg={3}></Col>
-      </Row>
-    
+    <Row className="Analiz">
+      <Col lg={3}></Col>
+      <Col xs={0} sm={0} md={0} lg={9} className="left-c">
+        <Image preview={false} src={grafik} alt="grafik" />
+      </Col>
+      <Col sm={24} md={24} lg={6} className="auth-c">
+        <PrimaryButton
+          className="buton1"
+          buttontext="Hedefleri Gör"
+          size="large"
+        ></PrimaryButton>
+        <PrimaryButton
+          className="buton2"
+          buttontext="Hedef Oluştur"
+        ></PrimaryButton>
+      </Col>
+      <Col lg={3}></Col>
+    </Row>
   );
 };
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./index.css";
 import HowItWorks from "../../page-parts/how-it-works/how-it-works";
 import Startpage from "../../page-parts/start-page/start-page";
-import Analysis from "../analysis-page/analysis-page";
 
 const Index = () => {
   return (
