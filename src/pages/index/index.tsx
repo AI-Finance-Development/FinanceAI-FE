cbc08234d5a99265f40c7182d9db26a624502bee
yagmurfdan <yagmurfdann@outlook.com>
2024-05-17 14:05:31 +0300

    css changed

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 import HowItWorks from "../../page-parts/how-it-works/how-it-works";
 import Startpage from "../../page-parts/start-page/start-page";
-import Analysis from "../analysis-page/analysis-page";
+
 
 const Index = () => {
   return (
@@ -15,6 +15,7 @@ const Index = () => {
       <br></br>
       <br></br>
       <br></br>
+     
     </>
   );
 };
