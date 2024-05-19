06a7ce9dc051d1b3deba324c7540a49119cdeb67
Okan Cezik <okacezik@gmail.com>
2024-05-19 13:58:40 +0300

    added grapfic

--- src/pages/analysis-page/analysis-page.tsx
+++ src/pages/analysis-page/analysis-page.tsx
@@ -1,9 +1,9 @@
-import { Row, Image, Col } from "antd";
+import { Row, Col } from "antd";
 import React, { useState } from "react";
-import grafik from "../../assets/grafik.png";
 import "./analysis-page.css";
 import TargetCard from "../../page-parts/target-card/target-card";
 import AddTargetModal from "../../components/modals/add-target-modal/add-target-modal";
+import FaiGraphic from "../../components/atomics/fai-graphic/fai-graphic";
 
 const Analysis = () => {
 
@@ -14,7 +14,7 @@ const Analysis = () => {
       <Row className="Analiz">
         <Col lg={3}></Col>
         <Col xs={0} sm={0} md={0} lg={9} className="left-c">
-          <Image preview={false} src={grafik} alt="grafik" />
+          <FaiGraphic />
         </Col>
         <Col sm={24} md={24} lg={6}>
           <TargetCard onClickFirstButton={() => { setOpenAddTargetModal(true) }} onClickSecondButton={() => { console.log("doki") }} />
