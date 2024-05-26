c85373fc726d3b057b0ba61d4d419c82e8d543d9
yagmurfdan <yagmurfdann@outlook.com>
2024-05-26 20:16:45 +0300

    404 page development

---
+++ src/pages/404-page/404-page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './404-page.css'; 
+
+const Errorpage = () => {
+
+
+  
+
+    return (
+        <div className="not-found-container">
+            <h1>404</h1>
+            <h2>Page Not Found</h2>
+            <p>The page you are looking for doesn't exist or has been moved.</p>
+            
+        </div>
+    );
+  
+}
+
+export default Errorpage
