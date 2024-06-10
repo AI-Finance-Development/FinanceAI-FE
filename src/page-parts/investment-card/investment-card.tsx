93297a31d53a31a266cbd0fd2624695740954239
Okan Cezik <okacezik@gmail.com>
2024-06-10 14:39:50 +0300

    hotfix

--- src/page-parts/investment-card/investment-card.tsx
+++ src/page-parts/investment-card/investment-card.tsx
@@ -3,18 +3,21 @@ import './investment-card.css'
 import FaiCard from '../../components/atomics/fai-card/fai-card'
 import { ReactComponent as DollarSVG } from '../../assets/svgs/dollar.svg';
 import PrimaryButton from '../../components/atomics/primary-button/primary-button';
-import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next'
 
-const InvestmentCard = () => {
-    const navigate = useNavigate()
+interface InvestmentCardProps{
+    title:string;
+    onClick:()=>void;
+}
+
+const InvestmentCard = (props:InvestmentCardProps) => {
     const { t } = useTranslation();
     return (
         <FaiCard style={{width:"70%"}}>
             <>
                 <DollarSVG />
-                <span className='card-message'>{t('investment.summary-and-goals')}</span>
-                <PrimaryButton buttontext={t('button.view-now')} size='large' onClick={()=>{navigate('/analysis')}}/>
+                <span className='card-message'>{props.title}</span>
+                <PrimaryButton buttontext={t('button.view-now')} size='large' onClick={props.onClick}/>
             </>
         </FaiCard>
     )
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import "./home-page.css";
 import { Col, Row } from "antd";
 import InvestmentCard from "../../page-parts/investment-card/investment-card";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+
+  const {t} = useTranslation();
+  const navigate = useNavigate()
+
   return (
     <Row className="home-container">
       <Col sm={0} lg={4}></Col>
       <Col sm={24} lg={8} style={{ display: "flex", justifyContent: "center" }}>
-        <InvestmentCard />
+        <InvestmentCard title='Harcama Analizi' onClick={()=>{navigate('/analysis')}}/>
       </Col>
       <Col sm={24} lg={8} style={{ display: "flex", justifyContent: "center" }}>
-        <InvestmentCard />
+        <InvestmentCard title={t('investment.summary-and-goals')} onClick={()=>navigate('/investment')}/>
       </Col>
       <Col sm={0} lg={4}></Col>
     </Row>
