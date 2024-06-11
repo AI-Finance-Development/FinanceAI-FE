9be93faafaff73281691cfd0afef17e822f9c265
yagmurfdan <yagmurfdann@outlook.com>
2024-06-12 00:36:18 +0300

    image idk

--- src/utils/image-handler.tsx
+++ src/utils/image-handler.tsx
@@ -5,7 +5,11 @@ import tuprs from '../assets/tupras-logo.png';
 import aselsan from '../assets/aselsan-logo.png';
 import pgsus from '../assets/pgss.png';
 import { Image, ImageProps } from 'antd';
-
+import google from '../assets/google.png';
+import meta from '../assets/meta.png';
+import microsoft from '../assets/microsof.png';
+import netflix from '../assets/netflix.png';
+import tesla from '../assets/tesla.png';
 interface ImageHandlerProps extends ImageProps{
     code:string;
 }
@@ -17,6 +21,11 @@ const ImageHandler = (props:ImageHandlerProps) => {
         props.code === 'PGSUS' ? pgsus:
         props.code === 'DOAS' ? doas:
         props.code === 'TUPRS' ? tuprs:
+        props.code === 'GOOGL' ? google:
+        props.code === 'TESLA' ? tesla:
+        props.code === 'META' ? meta:
+        props.code === 'NETFLİX' ? netflix:
+        props.code === 'MİCROSOFT' ? microsoft:
         aselsan 
     }
     height={"50 px"}
